b3f9c854e7cab15f9817ffcb222e41ed8e1a1b02
aseempurohit <purohitaseem@gmail.com>
2017-06-13 10:51:12 +0530

    Enhancements to support platform options
    Synchronized with updates in common file at client side

--- calculator_common.py
+++ calculator_common.py
@@ -2,6 +2,10 @@ import socket
 
 RECV_BUFF = 512
 SEPARATOR = ':'
+ADD = 1
+SUB = 2
+MULT = 3
+DIV = 4
 
 #Simulated enumeration function
 def enum(**named_values):
@@ -14,10 +18,11 @@ def get_id(str_len = 6):
 
 
 class SupportedFunction(object):
-    def __init__(self, code, string, handler):
+    def __init__(self, code, string, handler, validator = None):
         self.code = code
         self.string = string
         self.handler = handler
+        self.validator = validator
 
 
 class CalculatorPacket():
@@ -43,10 +48,12 @@ class CalculatorPacket():
 
 class CalculatorService(object):
 
-    def __init__(self, server_host, server_port, function_map):
+    def __init__(self, server_host, server_port, function_map, connection_handle = None, session_endpoint = None):
         self.remote_host = server_host
         self.remote_port = server_port
         self.function_map = function_map
+        self.external_conn_handle = connection_handle
+        self.session_endpoint = session_endpoint
         self.local_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
@@ -59,7 +66,7 @@ class CalculatorService(object):
             print('Received request buffer:{}'.format(request_data))
 
             decoded_list = CalculatorPacket.as_string(request_data)
-            
+
             #Get identifiers from request
             identifier = decoded_list[0]
             operation = decoded_list[1]
@@ -79,17 +86,35 @@ class CalculatorService(object):
     def execute(self, *args):
         identifier = get_id()
         packet = CalculatorPacket(identifier, *args)
-        
-        self.local_sock.sendto(packet.as_bytes(), (self.remote_host, self.remote_port))
 
-        calculated_packet_str, remote = self.local_sock.recvfrom(RECV_BUFF)
+        #import pdb;pdb.set_trace()
+        try:
+            calculated_packet_str = None
+            #Use ENS Client SDK provided handle, if available
+            if self.external_conn_handle:
+                #Edge Network Service: data transer & receipt - START
+                self.external_conn_handle.sendto(packet.as_bytes(), (self.session_endpoint.host, self.session_endpoint.port))
+                print('Data sent')
+                calculated_packet_str, remote = self.external_conn_handle.recvfrom(RECV_BUFF)
+                print('Response received')
+                #Edge Network Service: data transer & receipt - END
+            else:
+                self.local_sock.sendto(packet.as_bytes(), (self.remote_host, self.remote_port))
+                print('Data sent')
+                calculated_packet_str, remote = self.local_sock.recvfrom(RECV_BUFF)
+                print('Response received')
 
-        decoded_response_list = CalculatorPacket.as_string(calculated_packet_str)
+            if(calculated_packet_str != None and len(calculated_packet_str) > 0):
+                decoded_response_list = CalculatorPacket.as_string(calculated_packet_str)
 
-        if(decoded_response_list[0] == identifier):
-            return decoded_response_list[2]
-        else:
-            return 0
+                if(decoded_response_list[0] == identifier):
+                    return decoded_response_list[2]
+            else:
+                print('Invalid or empty response received')
+        except Exception as e:
+            print('Exception received during data transmission/receipt for calculation:{}'.format(e))
+
+        return 0
 
     def terminate(self):
         self.local_sock.close()
@@ -123,3 +148,10 @@ def division(*args):
 
     return result
 
+
+def input_validator(opcode, *args):
+    #Avoid divide by zero
+    if(opcode == DIV):
+        if 0 in args[1:]:
+            return False
+    return True
